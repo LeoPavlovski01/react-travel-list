115d2c14b089f56c0c5c59e2ebfd5f1bb76857dc
LeoPavlovski <leopavlovski01@gmail.com>
2026-01-19 17:38:25 +0100

    feat: Add mapping, displaying the items with ternary expressions.

--- src/App.js
+++ src/App.js
@@ -1,33 +1,60 @@
-export default function App(){
-return <div className='app'>
-    <Logo/>
-    <Form/>
-    <PackingList/>
-    <Stats/>
-</div>
+const initialItems = [
+  { id: 1, description: "Passports", quantity: 2, packed: false },
+  { id: 2, description: "Socks", quantity: 12, packed: false },
+  { id: 3, description: "Charger", quantity: 1, packed: true },
+];
+export default function App() {
+  return (
+    <div className="app">
+      <Logo />
+      <Form />
+      <PackingList />
+      <Stats />
+    </div>
+  );
 }
 
-function Logo(){
-    return (
-        <h1>🏝 Far Away 💼</h1>
-    )
+function Logo() {
+  return <h1>🏝 Far Away 💼</h1>;
 }
-function Form(){
-    return(
-        <div className='add-form'>
-            <h3>What do you need for your trip? 😍</h3>
-        </div>
-    )
+function Form() {
+  return (
+    <div className="add-form">
+      <h3>What do you need for your trip? 😍</h3>
+    </div>
+  );
 }
-function PackingList(){
-    return <div className='list'>List</div>
+
+function Item({ item }) {
+  return (
+    <li>
+      <span style={item.packed ? { textDecoration: "line-through" } : {}}>
+        {"  "}
+        {item.quantity}
+        {"  "}
+        {item.description}
+        <button>❌</button>
+      </span>
+    </li>
+  );
 }
-function Stats(){
-    return (
-        <footer className='stats'>
-            <em>
-           💼 You have X items on your list, and you already packed X (X%)
-            </em>
-        </footer>
-    )
+
+function PackingList() {
+  return (
+    <div className="list">
+      <ul>
+        {initialItems.map((item) => (
+          <Item key={item.description} item={item} />
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+function Stats() {
+  return (
+    <footer className="stats">
+      <em>💼 You have X items on your list, and you already packed X (X%)</em>
+    </footer>
+  );
 }
