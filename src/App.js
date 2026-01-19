5eb38ebc86a7a79551a4116f467344ac370b21eb
LeoPavlovski <leopavlovski01@gmail.com>
2026-01-19 17:58:29 +0100

    feat: Implemented the options with map, prevented the form from refreshing.

--- src/App.js
+++ src/App.js
@@ -18,10 +18,24 @@ function Logo() {
   return <h1>🏝 Far Away 💼</h1>;
 }
 function Form() {
+  function handleSubmit(e) {
+    e.preventDefault();
+    console.log(e);
+  }
+
   return (
-    <div className="add-form">
+    <form className="add-form" onSubmit={handleSubmit}>
       <h3>What do you need for your trip? 😍</h3>
-    </div>
+      <select>
+        {Array.from({ length: 20 }, (_, i) => i + 1).map((num) => (
+          <option value={num} key={num}>
+            {num}
+          </option>
+        ))}
+      </select>
+      <input type="text" placeholder="Item..." />
+      <button>Add</button>
+    </form>
   );
 }
 
@@ -44,7 +58,7 @@ function PackingList() {
     <div className="list">
       <ul>
         {initialItems.map((item) => (
-          <Item key={item.description} item={item} />
+          <Item key={item.id} item={item} />
         ))}
       </ul>
     </div>
